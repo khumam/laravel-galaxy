1dd9e87ebddf80443cb663e56a1d95c080690bbd
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-06-16 21:36:20 +0700

    up: setup auth page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,46 +1,36 @@
 <x-guest-layout>
+    <div class="mb-5">
+        <a href="{{ route('auth.google') }}" class="flex gap-3 font-medium bg-white px-6 rounded justify-center items-center py-3 border">Register with <img src="/icons/google-icon.svg" alt="Google icon" height="20" width="20"></a>
+    </div>
+    <div class="grid grid-cols-3 items-center gap-4 justify-between text-center mb-5">
+        <div class="h-[1px] bg-gray-300 w-full"></div>
+        <span>Or with email</span>
+        <div class="h-[1px] bg-gray-300 w-full"></div>
+    </div>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
-        <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Your fullname" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-
-        <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="yourname@yourmail.com"/>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" placeholder="Your strong password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
-        <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Repeat your password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="underline text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
